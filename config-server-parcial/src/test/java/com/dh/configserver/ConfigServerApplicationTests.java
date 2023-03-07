6023a2920582eedfab1664dca34ff3bf0f56f119
elkin.silva <elkinsilvamanrique@gmail.com>
2023-03-07 15:35:34 -0500

    Add eureka server

--- config-server-parcial/src/test/java/com/dh/configserver/ConfigServerApplicationTests.java
+++ config-server-parcial/src/test/java/com/dh/configserver/ConfigServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.dh.elkin.configserverparcial;
+package com.dh.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfigServerParcialApplicationTests {
+class ConfigServerApplicationTests {
 
     @Test
     void contextLoads() {

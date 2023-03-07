0388c207b226a5af94eece7cc289ae6185b9a7fb
elkin.silva <elkinsilvamanrique@gmail.com>
2023-03-07 15:17:43 -0500

    Add config server

---
+++ config-server-parcial/src/main/java/com/dh/elkin/configserverparcial/ConfigServerParcialApplication.java
@@ -0,0 +1,13 @@
+package com.dh.elkin.configserverparcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerParcialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerParcialApplication.class, args);
+    }
+
+}
---
+++ config-server-parcial/src/test/java/com/dh/elkin/configserverparcial/ConfigServerParcialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.elkin.configserverparcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerParcialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

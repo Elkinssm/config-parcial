fcc36e61738182c61f915371979d83a8190b772f
elkin.silva <elkinsilvamanrique@gmail.com>
2023-03-09 15:16:51 -0500

    Registro servidor eureka ms

--- movie-service/src/main/java/com/dh/movieservice/domain/model/Movie.java
+++ movie-service/src/main/java/com/dh/movieservice/domain/model/Movie.java
@@ -1,15 +1,9 @@
 package com.dh.movieservice.domain.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Basic;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import java.io.Serial;
 import java.io.Serializable;
 

b218b4c47d38734d2d5a2651837049defdbf43b2
Elkin Silva <elkinsilvamanrique@gmail.com>
2023-03-09 18:32:53 -0500

    Add feign client

--- catalog-service/src/main/java/com/dh/catalogservice/api/controller/CatalogController.java
+++ catalog-service/src/main/java/com/dh/catalogservice/api/controller/CatalogController.java
@@ -1,7 +1,10 @@
 package com.dh.catalogservice.api.controller;
 
-import com.dh.catalogservice.api.service.CatalogService;
+//import com.dh.catalogservice.api.service.CatalogService;
+
+import com.dh.catalogservice.api.client.IMovieServiceClient;
 import com.dh.catalogservice.domain.model.Movie;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,18 +16,25 @@ import java.util.List;
 @RestController
 @RequestMapping("/catalog")
 public class CatalogController {
-
-    private final CatalogService service;
-
-    public CatalogController(CatalogService service) {
-        this.service = service;
-    }
-
+    @Autowired
+    private IMovieServiceClient iMovieServiceClient;
 
     @GetMapping("/{genre}")
-    ResponseEntity<List<Movie>> getGenre(@PathVariable String genre) {
-
-        return ResponseEntity.ok().body(service.getMovieByGenre(genre));
+    ResponseEntity<List<Movie>> getCatalog(@PathVariable String genre) {
+        return iMovieServiceClient.getMoviesByCatalog(genre);
     }
+
+//    private final CatalogService service;
+//
+//    public CatalogController(CatalogService service) {
+//        this.service = service;
+//    }
+//
+//
+//    @GetMapping("/{genre}")
+//    ResponseEntity<List<Movie>> getGenre(@PathVariable String genre) {
+//
+//        return ResponseEntity.ok().body(service.getMovieByGenre(genre));
+//    }
 
 }
